a2561fa44b432f81bb0cf963bbedb927cc58ae48
Forrest <rainforest92@126.com>
2021-12-29 17:15:15 +0800

    更新 nasl config

--- javascript-example/src/quickstart.js
+++ javascript-example/src/quickstart.js
@@ -1,8 +1,8 @@
 const nasl = require('@lcap/asl');
 
-nasl.config.baseURL = 'http://defaulttenant.qa-ci.lcap.group';
-nasl.config.cookie = 'authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJc1NlcnZpY2VBY2NvdW50IjoiZmFsc2UiLCJVc2VySWQiOiIyOGRiMmUwNWY3ZTU0YzNiOTBhOWU0MWU1Y2ZlMjMxOCIsImV4cCI6MTYzOTY3NzQyNSwiSXNUaGlyZFBhcnR5Q29va2llIjoiZmFsc2UiLCJpYXQiOjE2Mzk2MzQyMjV9.HiQy2AFTWl5Wjk8YjlQROvvCp14NPz2EgabwnCFUCb4';
-const appId = '8c05b4d7-5928-491b-977d-212f1391904a';
+nasl.config.baseURL = 'http://defaulttenant.qa-ci.lcap.group'; // 低代码平台某租户的 URL
+nasl.config.cookie = 'authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJc1NlcnZpY2VBY2NvdW50IjoiZmFsc2UiLCJVc2VySWQiOiIyOGRiMmUwNWY3ZTU0YzNiOTBhOWU0MWU1Y2ZlMjMxOCIsImV4cCI6MTYzOTY3NzQyNSwiSXNUaGlyZFBhcnR5Q29va2llIjoiZmFsc2UiLCJpYXQiOjE2Mzk2MzQyMjV9.HiQy2AFTWl5Wjk8YjlQROvvCp14NPz2EgabwnCFUCb4'; // 登录后的 document.cookie
+const appId = '8c05b4d7-5928-491b-977d-212f1391904a'; // 需要访问的 appId
 
 async function start() {
     const app = new nasl.App({ id: appId });
--- typescript-example/src/quickstart.ts
+++ typescript-example/src/quickstart.ts
@@ -1,8 +1,8 @@
 import * as nasl from '@lcap/asl';
 
-nasl.config.baseURL = 'http://defaulttenant.qa-ci.lcap.group';
-nasl.config.cookie = 'authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJc1NlcnZpY2VBY2NvdW50IjoiZmFsc2UiLCJVc2VySWQiOiIyOGRiMmUwNWY3ZTU0YzNiOTBhOWU0MWU1Y2ZlMjMxOCIsImV4cCI6MTYzOTQ5MjY1MywiSXNUaGlyZFBhcnR5Q29va2llIjoiZmFsc2UiLCJpYXQiOjE2Mzk0NDk0NTN9.HrhsPCHN0TUpN8IQNl1OoHNJ7Z5mtwbIdbhHgZpEq0I';
-const appId = '2bb0bb6a-0fdd-4440-b5d2-a7c041da18f1';
+nasl.config.baseURL = 'http://defaulttenant.qa-ci.lcap.group'; // 低代码平台某租户的 URL
+nasl.config.cookie = 'authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJc1NlcnZpY2VBY2NvdW50IjoiZmFsc2UiLCJVc2VySWQiOiIyOGRiMmUwNWY3ZTU0YzNiOTBhOWU0MWU1Y2ZlMjMxOCIsImV4cCI6MTYzOTY3NzQyNSwiSXNUaGlyZFBhcnR5Q29va2llIjoiZmFsc2UiLCJpYXQiOjE2Mzk2MzQyMjV9.HiQy2AFTWl5Wjk8YjlQROvvCp14NPz2EgabwnCFUCb4'; // 登录后的 document.cookie
+const appId = '8c05b4d7-5928-491b-977d-212f1391904a'; // 需要访问的 appId
 
 async function start() {
     const app = new nasl.App({ id: appId });
